49c97a1d9a5a66e6a2321cadfd333f5d64bc3e4d
Unknown <ac_flood@hotmail.fr>
2017-09-25 14:38:52 +0200

    Cleanup project files
    Removes useless depdendencies & files

---
+++ CoreSWOAS/src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CoreSWOAS
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Main()
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    // options.ThreadCount = 4;
+                    // options.UseHttps("cert.pfx", "certpassword");
+                    options.NoDelay = true;
+                    options.UseConnectionLogging();
+                })
+                .UseUrls("http://+:5000" /*, "https://+:5001" */)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                //.UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
--- CoreSWOAS/src/Startup.cs
+++ CoreSWOAS/src/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,8 +6,15 @@ using Microsoft.Extensions.Logging;
 
 namespace CoreSWOAS
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="env"></param>
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -22,39 +25,39 @@ namespace CoreSWOAS
             Configuration = builder.Build();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        ///  This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="loggerFactory"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            app.UseMvc();
 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseSwagger();
+            app.UseSwaggerUi();
         }
     }
 }
